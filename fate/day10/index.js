85442d4469c361a62115da8524b3e0e574e129f1
Hot Chocolate <54476710+hotchocolate314@users.noreply.github.com>
2020-12-10 19:52:10 +0000

    Update index.js

--- fate/day10/index.js
+++ fate/day10/index.js
@@ -1,35 +1,46 @@
+var fs = require("fs");
+var text = fs.readFileSync("./input.txt", "utf-8");
+
+function main(data){
+    let current_total = 0, last_total = 0, prev1total = 0, prev2total = 0, prev3total = 0, sumThisIter = 0, diff1 = 0, diff3 = 1;
+    for(let i = 0; i < data.length; i++){
+    	prev3total = prev2total;
+    	prev2total = prev1total;
+    	prev1total = current_total;
+    	current_total = 0;
+        sumThisIter = 0;
+        
+        if(data[i-1] == 0) sumThisIter++;
+        if(data[i] - data[i-2] <= 3){
+            if(data[i-2] == 0) sumThisIter++;
+            sumThisIter += prev2total;
+        }
+
+        if(data[i] - data[i-3] <= 3){
+            if(data[i-3] == 0) sumThisIter++;
+            sumThisIter += prev3total;
+        }
+
+        current_total += sumThisIter + prev1total;
+        
+        switch(data[i] - (data[i-1] ? data[i-1] : 0)){
+            case 1:
+            	diff1++;
+            	break;
+            case 3:
+            	diff3++;
+	}
+            
+    }
+
+    console.log("Part 1: " + diff1*diff3);
+    console.log("Part 2: " + current_total);
+}
+
 function getData(text){
     const data= text.split("\n");
     data.pop();
-    return data.map(item => parseInt(item))
+    return [0, ...data.map(item => parseInt(item)).sort((a, b) => a-b)]
 }
 
-function findDifferences(data){
-    let diff1 = 0;
-    let diff2 = 0;
-    let diff3 = 1;
-    data = data.sort((a, b) => a - b);
-    for(let i = 0; i < data.length; i++){
-        let diff = 0;
-        if(i == 0){
-            diff = data[i];
-        }else{
-            diff = data[i] - data[i-1];
-        }
-        switch(diff){
-            case 1:
-                diff1++;
-                break;
-            case 2:
-                diff2++;
-                break;
-            case 3:
-                diff3++;
-                break;
-        }
-    }
-    return [diff1, diff2, diff3];
-}
-
-//let [diff1, diff2, diff3] = findDifferences(getData(document.getElementsByTagName("pre")[0].innerText));
-//console.log("Part 1 answer: " + diff1*diff3);
+main(getData(text));
