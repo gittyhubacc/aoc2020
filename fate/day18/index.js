43cdbddc390e0ef25c2d721a3a559c0e3863c797
Hot Chocolate <54476710+hotchocolate314@users.noreply.github.com>
2020-12-18 13:07:56 +0000

    Update index.js

--- fate/day18/index.js
+++ fate/day18/index.js
@@ -1,21 +1,3 @@
-function evaluate_p1(expr){
-    const matching_groups = expr.match(/\([0-9+*\/\-+ ]+\)/g);
-    if(matching_groups){
-        for(const group of matching_groups){
-            const evaluated = evaluate_p1(group.slice(1, group.length-1)).toString();
-            expr = expr.replace(group, evaluated);
-        }
-        if(expr.indexOf("(") !== -1) expr = evaluate_p1(expr).toString();
-    }
-    if(expr.search(/[*\/+\-\(\)]/) == -1) return parseInt(expr);
-    expr = expr.split(" ");
-    let retVal = expr[0];
-    for(let i = 1; i < expr.length; i+=2){
-        retVal = helper(retVal, expr[i+1], expr[i]);
-    }
-    return retVal;
-}
-
 function helper(a, b, sign){
     switch(sign){
         case "+":
@@ -25,23 +7,25 @@ function helper(a, b, sign){
     }
 }
 
-function evaluate_p2(expr){
+function evaluate(expr, part2){
     const matching_groups = expr.match(/\([0-9+*\/\-+ ]+\)/g);
     if(matching_groups){
         for(const group of matching_groups){
-            const evaluated = evaluate_p2(group.slice(1, group.length-1)).toString();
+            const evaluated = evaluate(group.slice(1, group.length-1), part2).toString();
             expr = expr.replace(group, evaluated);
         }
-        if(expr.indexOf("(") !== -1) expr = evaluate_p2(expr).toString();
+        if(expr.indexOf("(") !== -1) expr = evaluate(expr, part2).toString();
     }
-    if(expr.indexOf("*") !== -1){
-        const add_groups = expr.match(/[\d\*\+\(\)]+( \+ [\d\*\+\(\)]+)+/g);
-        if(add_groups){
-            for(const group of add_groups){
-                expr = expr.replace(group, "(" + group + ")");
+    if(part2){
+        if(expr.indexOf("*") !== -1){
+            const add_groups = expr.match(/[\d\*\+\(\)]+( \+ [\d\*\+\(\)]+)+/g);
+            if(add_groups){
+                for(const group of add_groups){
+                    expr = expr.replace(group, "(" + group + ")");
+                }
             }
+            if(expr.indexOf("(") !== -1) expr = evaluate(expr, part2).toString();
         }
-        if(expr.indexOf("(") !== -1) expr = evaluate_p2(expr).toString();
     }
     if(expr.search(/[*\/+\-\(\)]/) == -1) return parseInt(expr);
     expr = expr.split(" ");
@@ -55,8 +39,7 @@ function evaluate_p2(expr){
 function run(input, part2){
     input = input.split("\n");
     input.pop();
-    if(part2) return input.reduce((sum, current) => sum += evaluate_p2(current), 0);
-    return input.reduce((sum, current) => sum += evaluate_p1(current), 0);
+    return input.reduce((sum, current) => sum += evaluate(current, part2), 0);
 }
 
 console.log("Part 1: " + run(document.getElementsByTagName("pre")[0].innerText, false));
