1e44501ec7c06d93b475cf3ebd9dcac199f293e2
Hot Chocolate <54476710+hotchocolate314@users.noreply.github.com>
2020-12-21 10:10:39 +0000

    Small correction (wasn't alphabetically sorted before)

--- fate/day18/index.js
+++ fate/day18/index.js
@@ -1,34 +1,95 @@
-const helper = (a, b, sign) => sign == "+" ? parseInt(a)+parseInt(b) : parseInt(a)*parseInt(b);
+const fs = require("fs");
+const text = fs.readFileSync("./input.txt", "utf-8");
 
-function evaluate(expr, part2){
-    const matching_groups = expr.match(/\([0-9+*\/\-+ ]+\)/g);
-    if(matching_groups){
-        for(const group of matching_groups){
-            const evaluated = evaluate(group.slice(1, group.length-1), part2).toString();
-            expr = expr.replace(group, evaluated);
-        }
-    }
-    if(part2 && expr.indexOf("*") !== -1){
-        const add_groups = expr.match(/[\d\*\+\(\)]+( \+ [\d\*\+\(\)]+)+/g);
-        if(add_groups){
-            for(const group of add_groups)
-                expr = expr.replace(group, "(" + group + ")");
-        }
-    }
-    if(expr.indexOf("(") !== -1) expr = evaluate(expr, part2).toString();
-    if(expr.search(/[*\/+\-\(\)]/) == -1) return parseInt(expr);
-    expr = expr.split(" ");
-    let retVal = expr[0];
-    for(let i = 1; i < expr.length; i+=2)
-        retVal = helper(retVal, expr[i+1], expr[i]);
-    return retVal;
-}
-
-function run(input, part2){
-    input = input.split("\n");
+function get_data(input){
+    input = input.replace(/\)/g, "").split("\n");
     input.pop();
-    return input.reduce((sum, current) => sum += evaluate(current, part2), 0);
+    const ingredients = new Set();
+    const ingredients_array = [];
+    input = input.map(row => {
+        row = row.split(" (contains ")
+        row[0].split(" ").forEach(item => ingredients_array.push(item));
+        row[1] = row[1].split(", ");
+        row[1].forEach(item => ingredients.add(item));
+        return row;
+    })
+    return [ input, ingredients, ingredients_array ];
 }
 
-console.log("Part 1: " + run(document.getElementsByTagName("pre")[0].innerText, false));
-console.log("Part 2: " + run(document.getElementsByTagName("pre")[0].innerText, true));
+function union(setA, setB){
+    const common = new Set();
+    for(const item of setA) if(setB.has(item)) common.add(item);
+    return common;
+}
+
+function exclude(setA, setB){
+    const excluded = new Set();
+    for(const item of setB) if(!setA.has(item)) excluded.add(item);
+    return excluded;
+}
+
+function program(data){
+    const allergen_to_food = {};
+    for(const item of data[1]){ //loop over ingredients
+        for(const pair of data[0]){ //loop over food
+            if(pair[1].includes(item)){
+                if(!allergen_to_food[item]) allergen_to_food[item] = [];
+                allergen_to_food[item].push(new Set(pair[0].split(" ")));
+            }
+        }
+    }
+
+    const exclude_set = new Set();
+
+    const allergen_translate = {};
+    
+    for(const pair of Object.entries(allergen_to_food)){
+        let common = pair[1][0];
+        for(let i = 1; i < pair[1].length; i++){
+            common = union(common, pair[1][i]);
+        }
+        allergen_translate[pair[0]] = common;
+        if(common.size == 1){
+            exclude_set.add(Array.from(common)[0]);
+        }
+    }
+
+    let all_1 = false;
+    while(!all_1){
+        all_1 = true;
+        for(const pair of Object.entries(allergen_translate)){
+            if(pair[1].size == 1) continue;
+            allergen_translate[pair[0]] = exclude(exclude_set, pair[1]);
+            const new_set = allergen_translate[pair[0]];
+            
+            if(new_set.size == 1){
+                exclude_set.add(Array.from(new_set)[0]);
+            }else{
+                all_1 = false;
+            }
+        }
+    }
+
+    let non_allergens = 0;
+    for(const item of data[2]){
+        if(!exclude_set.has(item)) non_allergens++;
+    }
+
+    const english_to_other = {};
+
+    for(const pair of Object.entries(allergen_translate)){
+        english_to_other[Array.from(pair[1])[0]] = pair[0];
+    }
+
+    const ingredients_array = Array.from(exclude_set).sort((a, b) => {
+        return english_to_other[a] > english_to_other[b] ? 1 : -1;
+    });
+
+    const list = ingredients_array.join(",");
+
+    return [non_allergens, list];
+}
+
+const [ non_allergens, list ] = program(get_data(text));
+console.log("Part 1: "+ non_allergens);
+console.log("Part 2: " + list);
