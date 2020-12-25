1272417848cb6914acd4bce3bd06012314944b74
Hot Chocolate <54476710+hotchocolate314@users.noreply.github.com>
2020-12-25 14:55:11 +0000

    Update index.js

--- fate/day23/index.js
+++ fate/day23/index.js
@@ -1,56 +1,74 @@
-const input = "389125467";
+const input = "538914762";
 
-const arr = input.split("").map(item => parseInt(item));
-const original_length = arr.length;
-const sorted_array = arr.slice().sort((a, b) => a - b);
-const max = sorted_array[sorted_array.length-1];
-/*for(let i = arr.length; i < 1000000; i++){
-    arr.push(i - original_length + max);
-    sorted_array.push(i - original_length + max);
-}*/
+function get_data(input, part2){
+    const arr = input.split("").map(item => parseInt(item)-1); //array where each element is 1 less than in original
+    const original_length = arr.length;
+    const last = arr[arr.length-1];
+    
+    const links = [];
+    for(let i = 0; i < arr.length; i++)
+        links[arr[i]] = arr[mod(i+1, arr.length)];
+    
+    if(part2){
+        links[last] = 9;
+        for(let i = 9; i < 1000000; i++)
+            links[i] = i+1;
+        links[999999] = arr[0];
+    }
 
-function move_cups(current, cup_array){
-    const current_index = cup_array.indexOf(current);
-    const three_to_cut = [];
-    let end_insert_num = 0;
-    for(let i = 1; i <= 3; i++){
-        const cut_index = (current_index+i) % cup_array.length;
-        if(current_index < cut_index) end_insert_num++;
-        three_to_cut.push(cup_array[cut_index]);
-    }
-    const filtered = cup_array.filter(item => !three_to_cut.includes(item));
-    const insert_index = get_dest_index(current, cup_array, three_to_cut);
-    filtered.splice(insert_index, 0, ...three_to_cut);
-    if(filtered.indexOf(current) != current_index){
-        const end_insert = filtered.splice(0, end_insert_num);
-        filtered.splice(filtered.length, 0, ...end_insert);
-    }
-    return [filtered[mod(filtered.indexOf(current)+1, filtered.length)], filtered];
+    return [ arr[0], links ]
+}
+
+function move_cups(current, cups){
+    const next_3 = cups[current];
+
+    const exclude_three = [ next_3, cups[next_3] ];
+    exclude_three[2] = cups[exclude_three[1]];
+
+    const next_lowest = get_lowest(current, exclude_three, cups.length);
+    const item_after_next_lowest = cups[next_lowest];
+
+    cups[current] = cups[cups[cups[next_3]]];
+    cups[next_lowest] = next_3;
+
+    cups[cups[cups[cups[next_lowest]]]] = item_after_next_lowest;
+
+    return [ cups[current], cups ];
+}
+
+function get_lowest(current, exclude_three, cups_length){
+    while(exclude_three.includes(mod(--current, cups_length)));
+    return mod(current, cups_length);
 }
 
 function mod(num, n){
     return (num + n) % n;
 }
 
-function get_dest_index_no_filter(current, cup_array, three_elements){
-    let current_elem = three_elements[0];
-    let i = 0;
-    while(three_elements.includes(current_elem)){
-        current_elem = sorted_array[mod(sorted_array.indexOf(current) - ++i, sorted_array.length)];
+function get_in_answer_form(cups){
+    let retStr = "";
+    let current = 0;
+    while(cups[current] != 0){
+        current = cups[current];
+        retStr += current+1;
     }
-    return mod(cup_array.indexOf(current_elem) + 1, cup_array.length);
+    return retStr;
 }
 
-function get_in_answer_form(current, cup_array){
-    const end_insert = cup_array.splice(0, cup_array.indexOf(current));
-    cup_array.shift();
-    return [...cup_array, ...end_insert].join("");
+
+function program(input, part2){
+    let start = get_data(input, part2);
+    let iters = part2 ? 10000000 : 100;
+
+    for(let i = 0; i < iters; i++)
+        start = move_cups(...start);
+    
+    if(part2){
+        return (start[1][0]+1)*(start[1][start[1][0]]+1);
+    }else{
+        return get_in_answer_form(start[1])
+    }
 }
 
-let start = [ arr[0], arr ];
-for(let i = 0; i < 5; i++){
-    start = move_cups(...start);
-    console.log(start[1].reduce((str, current) => current == start[0] ? str + " (" + current + ") " : str + " " + current + " ", ""))
-}
-
-console.log(get_in_answer_form(1, start[1]))
+console.log("Part 1: " + program(input, false))
+console.log("Part 2: " + program(input, true))
